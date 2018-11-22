3c18877d2d0f5e93def730f89f684799b74d20b7
tiago.romano <tiago.romano@gmail.com>
2018-11-22 23:10:57 +0000

    First commit...

---
+++ src/main/java/app/entity/Cidade.java
@@ -0,0 +1,140 @@
+package app.entity;
+
+import java.io.*;
+import javax.persistence.*;
+import java.util.*;
+import javax.xml.bind.annotation.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFilter;
+import cronapi.rest.security.CronappSecurity;
+
+
+/**
+ * Classe que representa a tabela CIDADE
+ * @generated
+ */
+@Entity
+@Table(name = "\"CIDADE\"")
+@XmlRootElement
+@CronappSecurity
+@JsonFilter("app.entity.Cidade")
+public class Cidade implements Serializable {
+
+  /**
+   * UID da classe, necessário na serialização
+   * @generated
+   */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * @generated
+   */
+  @Id
+  @Column(name = "id", nullable = false, insertable=true, updatable=true)
+  private java.lang.Integer id;
+
+  /**
+  * @generated
+  */
+  @Column(name = "nome", nullable = true, unique = false, insertable=true, updatable=true)
+  
+  private java.lang.String nome;
+
+  /**
+  * @generated
+  */
+  @Column(name = "estado", nullable = true, unique = false, insertable=true, updatable=true)
+  
+  private java.lang.Integer estado;
+
+  /**
+   * Construtor
+   * @generated
+   */
+  public Cidade(){
+  }
+
+
+  /**
+   * Obtém id
+   * return id
+   * @generated
+   */
+  
+  public java.lang.Integer getId(){
+    return this.id;
+  }
+
+  /**
+   * Define id
+   * @param id id
+   * @generated
+   */
+  public Cidade setId(java.lang.Integer id){
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Obtém nome
+   * return nome
+   * @generated
+   */
+  
+  public java.lang.String getNome(){
+    return this.nome;
+  }
+
+  /**
+   * Define nome
+   * @param nome nome
+   * @generated
+   */
+  public Cidade setNome(java.lang.String nome){
+    this.nome = nome;
+    return this;
+  }
+
+  /**
+   * Obtém estado
+   * return estado
+   * @generated
+   */
+  
+  public java.lang.Integer getEstado(){
+    return this.estado;
+  }
+
+  /**
+   * Define estado
+   * @param estado estado
+   * @generated
+   */
+  public Cidade setEstado(java.lang.Integer estado){
+    this.estado = estado;
+    return this;
+  }
+
+  /**
+   * @generated
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+    Cidade object = (Cidade)obj;
+    if (id != null ? !id.equals(object.id) : object.id != null) return false;
+    return true;
+  }
+
+  /**
+   * @generated
+   */
+  @Override
+  public int hashCode() {
+    int result = 1;
+    result = 31 * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+}
